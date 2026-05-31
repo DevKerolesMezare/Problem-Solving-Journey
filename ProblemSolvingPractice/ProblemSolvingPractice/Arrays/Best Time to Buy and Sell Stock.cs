69f23b8d3178cd52a5b250ecb00ca6a0e8b3e550
Keroles Mezare <keroles.mezare2005@gmail.com>
2026-06-01 02:12:10 +0300

    Day 8:  ├── Maximum Subarra  ├── Best Time to Buy and Sell Stock

---
+++ ProblemSolvingPractice/ProblemSolvingPractice/Arrays/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingPractice.Arrays
+{
+    internal class Best_Time_to_Buy_and_Sell_Stock
+    {
+        // DONE>>!
+        // حليتها من قبل
+
+        //121. Best Time to Buy and Sell Stock
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = prices[0];
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int CurrentPrice = prices[i] - minPrice;
+                minPrice = Math.Min(minPrice, prices[i]);
+
+                if (CurrentPrice > maxProfit)
+                    maxProfit = CurrentPrice;
+            }
+            return maxProfit;
+        }
+    }
+}
---
+++ ProblemSolvingPractice/ProblemSolvingPractice/Arrays/Maximum Subarray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingPractice.Arrays
+{
+    internal class Maximum_Subarray
+    {
+        /// DONE...! Kadane Algorithm 
+        ///  Kadaneتم هيا هيا فكره ال
+        ///  
+
+        // 53. Maximum Subarray
+        public int MaxSubArray(int[] nums)
+        {
+            int current = 0;
+            int max = int.MinValue;
+
+            foreach (int num in nums)
+            {
+                current = Math.Max(num, current + num);
+                max = Math.Max(max, current);
+            }
+            return max;
+        }
+    }
+}
