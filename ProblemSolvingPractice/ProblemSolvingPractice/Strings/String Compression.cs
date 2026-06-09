9936824845bfb242b45f23ca21037bb0a92a46c7
Keroles Mezare <keroles.mezare2005@gmail.com>
2026-06-09 05:16:57 +0300

    Day 14: String Compression

---
+++ ProblemSolvingPractice/ProblemSolvingPractice/Strings/String Compression.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingPractice.Strings
+{
+    internal class String_Compression
+    {
+        // حل اول
+        //443. String Compression
+        public int Compress(char[] chars)
+        {
+            if (chars.Length == 1)
+                return 1;
+
+            char c = chars[0];
+            int count = 1;
+            int write_index = 0;
+
+            for (int i = 1; i < chars.Length; i++)
+            {
+                if (c == chars[i])
+                    count++;
+                else
+                {
+                    if (count > 1)
+                    {
+                        chars[write_index++] = c;
+                        if (count > 9)
+                        {
+                            foreach (char digit in count.ToString())
+                            {
+                                chars[write_index++] = digit;
+                            }
+                        }
+                        else
+                        {
+                            chars[write_index++] = (char)(count +'0');
+                        }
+                    }
+                    else
+                    {
+                        chars[write_index++] = c;
+                    }
+                    c = chars[i];
+                    count = 1;
+                }
+            }
+
+            if (count > 1)
+            {
+                chars[write_index++] = c;
+                if (count > 9)
+                {
+                    foreach (char digit in count.ToString())
+                    {
+                        chars[write_index++] = digit;
+                    }
+                }
+                else
+                {
+                    chars[write_index++] = (char)(count +'0');
+                }
+            }
+            else
+            {
+                chars[write_index++] = c;
+            }
+
+            return write_index;
+        }
+
+    }
+}
